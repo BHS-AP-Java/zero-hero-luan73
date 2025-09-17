576769c3f90e5be8069e885255b289b25dcd08f1
luan73 <115661220+luan73@users.noreply.github.com>
2025-09-17 16:13:59 +0000

    update 9:13 9/17/24

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -10,13 +10,31 @@
 
 package edu.bhscs;
 
-class Main {
+public class Main {
+
+  public static float NAME_OF_THIS_FLOAT = 69.42f;
+  public static char A = 'a';
+
   public static void main(String[] args) {
-    System.out.println("-------------------");
-    System.out.println("!!!! PROGRAM START !!!!");
-    System.out.println("hello");
-    System.out.println("!!!!PROGRAM END!!!!");
-    System.out.println("-------------------");
+    System.out.println(A);
+    System.out.println(pushup("garlic"));
+    System.out.println(pushup("garlic"));
+    System.out.println(pushup("garlic"));
+    System.out.println(pushup("garlic"));
+
+    pushup("garlic");
+    pushup("garlic");
+    pushup("garlic");
+    pushup("garlic");
+  Awesome steve = new Awesome("steve");
+  Awesome sue = new Awesome("sue");
+  Awesome linda = new Awesome("L");
+  Awesome jhon = new Awesome("jhon");
+System.out.println( steve.getName() );
+
   }
 
+  public static String pushup(String food) {
+    return "sweat" + food;
+  }
 }
